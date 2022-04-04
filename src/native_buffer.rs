244f6594a2c476f1af9c66c1b457cc90ea6d0863
tzz <koi_town@outlook.com>
2022-04-04 22:26:47 +0800

    update native_buffer pipeline_state

--- src/native_buffer.rs
+++ src/native_buffer.rs
@@ -3,9 +3,13 @@ vertex_buffer::{VertexBuffer,VertexAttributeDataType},
 pipeline_state::{PipelineState},
 };
 
+use glow::HasContext;
+
+
 use std::{
     marker::PhantomData,
 };
+use std::mem::size_of;
 
 
 pub struct NativeBuffer {
@@ -65,6 +69,59 @@ impl NativeBufferBuilder{
             data_size: buffer.raw_data().len(),
         }
     }
+
+
+    fn build(self, state: &mut PipelineState) -> Result<NativeBuffer, FrameworkError> {
+        let vbo = unsafe { state.gl.create_buffer()? };
+
+        state.set_vertex_buffer_object(Some(vbo));
+
+        if self.data_size > 0 {
+            unsafe {
+                state.gl.buffer_data_u8_slice(
+                    glow::ARRAY_BUFFER,
+                    std::slice::from_raw_parts(self.data, self.data_size),
+                    self.kind as u32,
+                );
+            }
+        }
+
+        let native_buffer = NativeBuffer {
+            state,
+            id: vbo,
+            kind: self.kind,
+            element_size: self.element_size,
+            size_bytes: self.data_size,
+            thread_mark: Default::default(),
+        };
+
+        let mut offset = 0usize;
+        for definition in self.attributes {
+            unsafe {
+                state.gl.vertex_attrib_pointer_f32(
+                    definition.location,
+                    definition.kind.length() as i32,
+                    definition.kind.get_type(),
+                    definition.normalized,
+                    self.element_size as i32,
+                    offset as i32,
+                );
+                state
+                    .gl
+                    .vertex_attrib_divisor(definition.location, definition.divisor);
+                state.gl.enable_vertex_attrib_array(definition.location);
+
+                offset += definition.kind.size_bytes();
+
+                if offset > self.element_size {
+                    state.set_vertex_buffer_object(Default::default());
+                    return Err(FrameworkError::InvalidAttributeDescriptor);
+                }
+            }
+        }
+
+        Ok(native_buffer)
+    }
 }
 
 pub struct AttributeDefinition {
@@ -75,6 +132,7 @@ pub struct AttributeDefinition {
 }
 
 
+#[derive(Copy, Clone)]
 pub enum AttributeKind {
     Float,
     Float2,
@@ -98,9 +156,174 @@ pub enum AttributeKind {
 }
 
 
+impl AttributeKind {
+    pub fn size_bytes(self) -> usize {
+        match self {
+            AttributeKind::Float => size_of::<f32>(),
+            AttributeKind::Float2 => size_of::<f32>() * 2,
+            AttributeKind::Float3 => size_of::<f32>() * 3,
+            AttributeKind::Float4 => size_of::<f32>() * 4,
+
+            AttributeKind::UnsignedByte => size_of::<u8>(),
+            AttributeKind::UnsignedByte2 => size_of::<u8>() * 2,
+            AttributeKind::UnsignedByte3 => size_of::<u8>() * 3,
+            AttributeKind::UnsignedByte4 => size_of::<u8>() * 4,
+
+            AttributeKind::UnsignedShort => size_of::<u16>(),
+            AttributeKind::UnsignedShort2 => size_of::<u16>() * 2,
+            AttributeKind::UnsignedShort3 => size_of::<u16>() * 3,
+            AttributeKind::UnsignedShort4 => size_of::<u16>() * 4,
+
+            AttributeKind::UnsignedInt => size_of::<u32>(),
+            AttributeKind::UnsignedInt2 => size_of::<u32>() * 2,
+            AttributeKind::UnsignedInt3 => size_of::<u32>() * 3,
+            AttributeKind::UnsignedInt4 => size_of::<u32>() * 4,
+        }
+    }
+
+    fn get_type(self) -> u32 {
+        match self {
+            AttributeKind::Float
+            | AttributeKind::Float2
+            | AttributeKind::Float3
+            | AttributeKind::Float4 => glow::FLOAT,
+
+            AttributeKind::UnsignedByte
+            | AttributeKind::UnsignedByte2
+            | AttributeKind::UnsignedByte3
+            | AttributeKind::UnsignedByte4 => glow::UNSIGNED_BYTE,
+
+            AttributeKind::UnsignedShort
+            | AttributeKind::UnsignedShort2
+            | AttributeKind::UnsignedShort3
+            | AttributeKind::UnsignedShort4 => glow::UNSIGNED_SHORT,
+
+            AttributeKind::UnsignedInt
+            | AttributeKind::UnsignedInt2
+            | AttributeKind::UnsignedInt3
+            | AttributeKind::UnsignedInt4 => glow::UNSIGNED_INT,
+        }
+    }
+
+    fn length(self) -> usize {
+        match self {
+            AttributeKind::Float
+            | AttributeKind::UnsignedByte
+            | AttributeKind::UnsignedShort
+            | AttributeKind::UnsignedInt => 1,
+
+            AttributeKind::Float2
+            | AttributeKind::UnsignedByte2
+            | AttributeKind::UnsignedShort2
+            | AttributeKind::UnsignedInt2 => 2,
+
+            AttributeKind::Float3
+            | AttributeKind::UnsignedByte3
+            | AttributeKind::UnsignedShort3
+            | AttributeKind::UnsignedInt3 => 3,
+
+            AttributeKind::Float4
+            | AttributeKind::UnsignedByte4
+            | AttributeKind::UnsignedShort4
+            | AttributeKind::UnsignedInt4 => 4,
+        }
+    }
+}
+
+
 #[derive(Copy, Clone)]
 #[repr(u32)]
 pub enum GeometryBufferKind {
     StaticDraw = glow::STATIC_DRAW,
     DynamicDraw = glow::DYNAMIC_DRAW,
+}
+
+
+/// Set of possible renderer errors.
+#[derive(Debug, thiserror::Error)]
+pub enum FrameworkError {
+    #[error(
+        "Compilation of \"{}\" shader has failed: {}",
+        shader_name,
+        error_message
+    )]
+    /// Compilation of a shader has failed.
+    ShaderCompilationFailed {
+        /// Name of shader.
+        shader_name: String,
+        /// Compilation error message.
+        error_message: String,
+    },
+    /// Means that shader link stage failed, exact reason is inside `error_message`
+    #[error("Linking shader \"{}\" failed: {}", shader_name, error_message)]
+    ShaderLinkingFailed {
+        /// Name of shader.
+        shader_name: String,
+        /// Linking error message.
+        error_message: String,
+    },
+    /// Shader source contains invalid characters.
+    #[error("Shader source contains invalid characters")]
+    FaultyShaderSource,
+    /// There is no such shader uniform (could be optimized out).
+    #[error("There is no such shader uniform: {0}")]
+    UnableToFindShaderUniform(String),
+    /// Texture has invalid data - insufficient size.
+    #[error(
+        "Texture has invalid data (insufficent size): expected {}, actual: {}",
+        expected_data_size,
+        actual_data_size
+    )]
+    InvalidTextureData {
+        /// Expected data size in bytes.
+        expected_data_size: usize,
+        /// Actual data size in bytes.
+        actual_data_size: usize,
+    },
+    /// None variant was passed as texture data, but engine does not support it.
+    #[error("None variant was passed as texture data, but engine does not support it.")]
+    EmptyTextureData,
+    /// Means that you tried to draw element range from GeometryBuffer that
+    /// does not have enough elements.
+    #[error(
+        "Tried to draw element from GeometryBuffer that does not have enough elements:
+        start: {},
+        end: {},
+        total: {}
+        ",
+        start,
+        end,
+        total
+    )]
+    InvalidElementRange {
+        /// First index.
+        start: usize,
+        /// Last index.
+        end: usize,
+        /// Total amount of triangles.
+        total: usize,
+    },
+    /// Means that attribute descriptor tries to define an attribute that does
+    /// not exists in vertex, or it does not match size. For example you have vertex:
+    ///   pos: float2,
+    ///   normal: float3
+    /// But you described second attribute as Float4, then you'll get this error.
+    #[error("An attribute descriptor tried to define an attribute that does not exist in vertex or doesn't match size.")]
+    InvalidAttributeDescriptor,
+    /// Framebuffer is invalid.
+    #[error("Framebuffer is invalid")]
+    InvalidFrameBuffer,
+    /// OpenGL failed to construct framebuffer.
+    #[error("OpenGL failed to construct framebuffer.")]
+    FailedToConstructFBO,
+    /// Custom error. Usually used for internal errors.
+    #[error("Custom error: {0}")]
+    Custom(String),
+}
+
+
+impl From<String> for FrameworkError {
+    fn from(v: String) -> Self {
+        Self::Custom(v)
+    }
 }
--- src/pipeline_state.rs
+++ src/pipeline_state.rs
@@ -1,2 +1,69 @@
+use glow::HasContext;
+use std::fmt::Display;
 pub struct PipelineState {
+    pub gl: glow::Context,
+    vao: Option<glow::VertexArray>,
+    frame_statistics: PipelineStatistics,
+    vbo: Option<glow::Buffer>,
+
+}
+use std::fmt::Formatter;
+
+impl PipelineState{
+    pub fn set_vertex_array_object(&mut self, vao: Option<glow::VertexArray>) {
+        if self.vao != vao {
+            self.vao = vao;
+
+            self.frame_statistics.vao_binding_changes += 1;
+
+            unsafe {
+                self.gl.bind_vertex_array(self.vao);
+            }
+        }
+    }
+
+    pub fn set_vertex_buffer_object(&mut self, vbo: Option<glow::Buffer>) {
+        if self.vbo != vbo {
+            self.vbo = vbo;
+
+            self.frame_statistics.vbo_binding_changes += 1;
+
+            unsafe {
+                self.gl.bind_buffer(glow::ARRAY_BUFFER, self.vbo);
+            }
+        }
+    }
+}
+
+
+#[derive(Debug, Default, Copy, Clone)]
+pub struct PipelineStatistics {
+    pub texture_binding_changes: usize,
+    pub vbo_binding_changes: usize,
+    pub vao_binding_changes: usize,
+    pub blend_state_changes: usize,
+    pub framebuffer_binding_changes: usize,
+    pub program_binding_changes: usize,
+}
+
+
+impl Display for PipelineStatistics {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Pipeline state changes:\n\
+            \tTextures: {},\n\
+            \tVBO: {},\n\
+            \tVAO: {},\n\
+            \tFBO: {},\n\
+            \tShaders: {},\n\
+            \tBlend: {}",
+            self.texture_binding_changes,
+            self.vbo_binding_changes,
+            self.vao_binding_changes,
+            self.framebuffer_binding_changes,
+            self.program_binding_changes,
+            self.blend_state_changes
+        )
+    }
 }
